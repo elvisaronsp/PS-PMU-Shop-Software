b7edaa92d7b25bc867a5a6206af5767b55b358bb
iwelina-popova <iwelina.popova@gmail.com>
2015-04-19 17:33:30 +0300

    Connect ProductClasses.dbml with ProductData.cs

---
+++ WPF - Shop system/Data/ProductData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF___Shop_system.Data
+{
+    public class ProductData
+    {
+        public static string ProductStatus(string productID)
+        {
+            ProductClassesDataContext currectProduct = new ProductClassesDataContext();
+            var product = currectProduct.GetTable<Product>()
+                          .Where(pr => pr.UNumber == productID)
+                          .FirstOrDefault();
+
+            if (product != null)
+            {
+                return "Product found!";
+            }
+            else
+            {
+                return "Product is not found!";
+            }
+        }
+
+        public Product ProductSearching(string productName)
+        {
+            ProductClassesDataContext currectProduct = new ProductClassesDataContext();
+            var product = currectProduct.GetTable<Product>()
+                          .Where(pr => pr.Name == productName)
+                          .FirstOrDefault();
+
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
